2f5a4a641e20a33834f2a42bc71893846a7050fb
Fanzil <fan.hakimov2010@yandex.ru>
2019-12-26 22:32:09 +0500

    Remove app.py to the yandex_music folder

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from .yandex_music import app, db
-from .yandex_music import models, view
-from .yandex_music.users.blueprint import users
--- yandex_music/app.py
+++ yandex_music/app.py
@@ -1,6 +1,6 @@
 from . import app, db
 from . import models, view
-from . import users
+from .users.blueprint import users
 
 
 app.register_blueprint(users, url_prefix="/users")
